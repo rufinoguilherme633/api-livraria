44533952f7452a436e072c4f6f97846d6a7a3505
Guilherme Rufino <110607952+rufinoguilherme633@users.noreply.github.com>
2023-09-01 11:24:40 -0300

    api livraria

---
+++ app/src/main/java/com/example/retrofit_api_livraria/Cadastro_Categoria.kt
@@ -0,0 +1,11 @@
+package com.example.retrofit_api_livraria
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cadastro_Categoria : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_categoria)
+    }
+}
